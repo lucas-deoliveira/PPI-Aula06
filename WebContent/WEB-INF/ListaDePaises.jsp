718a995f498104af421d6bf43105e2d0274f4e73
lucas-deoliveira <eng.oliveiralucas@hotmail.com>
2020-04-15 21:06:14 -0300

    Aula 06

---
+++ WebContent/WEB-INF/ListaDePaises.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="model.Pais, java.util.ArrayList" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Lista de Paises</title>
+</head>
+<body>
+<%
+	ArrayList<Pais> paises = (ArrayList<Pais>)request.getAttribute("paises");
+	for(Pais pais:paises){
+%>
+<p>
+<label>ID:</label> <%=pais.getId() %><br>
+<label>Nome:</label> <%=pais.getNome() %><br>
+<label>POPULAÇAO:</label> <%=pais.getPopulacao() %><br>
+<label>AREA:</label> <%=pais.getArea() %>
+</p>
+<%} %>
+</body>
+</html>
---
+++ src/controller/ManterPaisController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+@WebServlet("/ManterPais.do")
+public class ManterPaisController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String pNome = request.getParameter("nome");
+		String pPopulacao = request.getParameter("populacao");
+		String pArea = request.getParameter("area");
+		String acao = request.getParameter("acao");
+
+//instanciar o javabean
+		Pais pais = new Pais();
+		pais.setNome(pNome);
+		pais.setPopulacao(Long.parseLong(pPopulacao));
+		pais.setArea(Double.parseDouble(pArea));
+
+		PaisService cs = new PaisService();
+		RequestDispatcher dispatcher = null;
+
+		switch (acao) {
+		case "Incluir":
+			cs.criar(pais);
+			pais = cs.consultar(pais.getId());
+			
+			//manda parametro para o JSP via request
+			request.setAttribute("pais", pais);
+			dispatcher = request.getRequestDispatcher("Pais.jsp");
+			break;
+		case "Listar":
+			ArrayList<Pais> paises = cs.listarTodos();
+			request.setAttribute("paises", paises);
+			dispatcher = request.getRequestDispatcher("ListaDePaises.jsp");
+		}
+
+
+		dispatcher.forward(request, response);
+	}
+}
